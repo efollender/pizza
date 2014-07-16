674b229ef07c7eacf07b04cad98a47433b31727c
Emma Follender <efollender@gmail.com>
2014-07-16 10:37:55 -0500

    Added methods deliver! and late? to set the delivery time for a pizza and to check if it's late

--- pizza.rb
+++ pizza.rb
@@ -3,6 +3,23 @@ class Pizza
   def initialize(toppings=[Topping.new('cheese', vegetarian:true)])
     @toppings = toppings
   end
+  def vegetarian?
+    vegetarian = @toppings.first.vegetarian
+    @toppings.all? {|x| x.vegetarian == true}
+  end
+  def add_topping(new_topping)
+    @toppings << new_topping
+  end
+  def deliver!
+    @delivery_time = Time.now + 30*60
+  end
+  def late?(now=Time.now)
+    if @delivery_time < now
+      return true
+    else
+      return false
+    end
+  end
 end
 
 class Topping
@@ -12,5 +29,3 @@ class Topping
     @vegetarian = vegetarian
   end
 end
-
-
